fafe8d060c9ad4876edcc8cbc9af23b39318e9e1
Soloveiko-Ptaha <nata-li-88@mail.ru>
2016-04-15 00:38:38 +0300

    RWD

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 $(function(){
+//Pop-up window
 	$('.js_open_popup').click(function(e){
 		e.preventDefault();
 	    $('.js_popap').show();
@@ -6,9 +7,29 @@ $(function(){
 	$('.js_close').click(function(){
 		$('.js_popap').hide();
 	});
-	
+// Panel expand	
 	$('.js_panel-expand').click(function(){
 		$('.js_user-menu').show(300);
 		$('.js_panel-expand').hide();
 	});
+
+//Slider
+	$('.slider').slick({
+	  	dots: false,
+	 	infinite: true,
+		// speed: 300,
+	    arrows: false,
+	    responsive: [{
+	    	breakpoint: 1024,
+	    	settings: {
+	      	slidesToShow: 1,
+	        slidesToScroll: 1,
+	        infinite: true,
+	        dots: true
+		    }
+		},]
+	});
+
 })
+
+
